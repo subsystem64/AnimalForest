d6592727fbb02e2f60b485a23e529c9140ad62f7
David Fontes <dsaldanhadaluzfontes@ucsd.edu>
2026-05-01 11:45:45 -0700

    screen size

--- ant.gd
+++ ant.gd
@@ -4,8 +4,12 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AntSprite.play("idle")
-	
 
+func _mouse_enter() -> void:
+	$AntSprite.play("walk")
+
+func _mouse_exit() -> void:
+	$AntSprite.play("idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
